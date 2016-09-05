9784a6c87e5c59058fefdd922984d71cfd04e185
Rahul Chatterjee <rahul@zenbook.com>
2016-09-05 10:36:21 -0400

    Commiting to see what happens

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
-from setuptools import setup
-from Cython.Build import cythonize
-import os
+import warnings
+try:
+    from Cython.Distutils import build_ext
+    from setuptools import setup, Extension
+    HAVE_CYTHON = True
+except ImportError as e:
+    HAVE_CYTHON = False
+    warnings.warn(e.message)
+    from distutils.core import setup, Extension
+    from distutils.command import build_ext
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+keyboardpyx = Extension(
+    '_keyboard',
+    sources = ['word2keypress/_keyboard.pyx'],
+    include_dirs = ['word2keypress/']
+)
 
-setup(
+configuration = dict(
     name='word2keypress',
     version='1.1',
     description='Convert word to keypress sequence',
@@ -14,16 +24,46 @@ setup(
     url='https://github.com/rchatterjee/word2keypress.git',
     download_url = 'https://github.com/rchatterjee/word2keypress/tarball/v1.1',
     install_requires=[
-        'cython',
         'numpy',
         'python-levenshtein'
     ],
     long_description='See README.md',
     packages=['word2keypress'],
-    ext_modules=cythonize('word2keypress/_keyboard.pyx'),  # accepts a glob pattern
+    ext_modules=[keyboardpyx], # cythonize('word2keypress/_keyboard.pyx'),  # accepts a glob pattern
+    cmdclass={'build_ext': build_ext},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License"
-    ],
+    ]
 )
+
+
+if not HAVE_CYTHON:
+    keyboardpyx.sources[0] = '_keyboard.c'
+
+
+setup(**configuration)
+
+
+# from setuptools import setup, extension
+# from Cython.Build import cythonize
+
+# try:
+#     from Cython.Distutils import build_ext
+# except ImportError:
+#     use_cython = False
+# else:
+#     use_cython = True
+
+# import os
+
+# def read(fname):
+#     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# ext = extension.Extension(
+#     'word2keypress/_keyboard', 
+#     ['word2keypress/_keyboard.pyx']
+# )
+
+# setup(
+# )
