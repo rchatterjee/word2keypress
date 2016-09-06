bb4fa92f60e805fe98f0eeb132df707a5e15ee39
Rahul Chatterjee <rahul@cs.cornell.edu>
2016-09-06 14:24:11 -0400

    transpose distance

--- word2keypress/__init__.py
+++ word2keypress/__init__.py
@@ -1,11 +1,18 @@
 from _keyboard import Keyboard
-import Levenshtein as lv
-
 kb = Keyboard(u'US')
+
+try:
+    from pyxdameraulevenshtein import \
+        normalized_damerau_levenshtein_distance as _distance
+except ImportError:
+    import Levenshtein.distance as _distance
+
 def distance(w1, w2):
-    # print kb.print_keyseq(kb.word_to_keyseq(unicode(w1))),
-    # print kb.print_keyseq(kb.word_to_keyseq(unicode(w2)))
-    return lv.distance(
+    """
+    Computes the edit (DL edit) distance between two strings @s1 and @s2.
+    Edits are: [insert, delete, replace, transpose]
+    """
+    return _distance(
         kb.word_to_keyseq(unicode(w1)),
         kb.word_to_keyseq(unicode(w2))
     )
